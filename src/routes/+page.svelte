b12695b5c4b1c9533eb61158f65d15792dda2426
Masaru Nohara <masaru.nohara@tohmatsu.co.jp>
2025-02-23 00:38:02 +0900

    [fix] icon color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,10 +17,10 @@
 
 <div class='min-h-screen flex flex-col items-center justify-center p-4 bg-base'>
   <div class='text-center mb-8'>
-    <h1 class='text-4xl font-bold text-indigo-800 mb-2'>
-      Are you free? (Design 3)
+    <h1 class='text-4xl font-bold text-sky-800 mb-2'>
+      Are you free?
     </h1>
-    <p class='text-indigo-600'>Find the perfect time to meet</p>
+    <p class='text-sky-600'>Schedule your free time</p>
   </div>
   <svelte:component this={clockIcon} size={120} />
   <div class='flex justify-center items-center mt-6 space-x-2'>
