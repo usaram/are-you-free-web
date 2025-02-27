20ea1a6f182ab578e3f30418f6f1c4ed7a194197
Masaru Nohara <masaru.nohara@tohmatsu.co.jp>
2025-02-27 19:33:19 +0900

    [fix] deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'path'
 import { sveltekit } from '@sveltejs/kit/vite'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
