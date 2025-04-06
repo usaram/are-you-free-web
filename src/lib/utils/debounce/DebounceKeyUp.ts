676d517a16cc1360f36418dfea596cefe3591fdb
Masaru Nohara <masaru.nohara@tohmatsu.co.jp>
2025-04-06 21:12:03 +0900

    [fix] eslint

--- src/lib/utils/debounce/DebounceKeyUp.ts
+++ src/lib/utils/debounce/DebounceKeyUp.ts
@@ -4,10 +4,15 @@
  *
  * @param input - キーアップイベント時に実行する関数
  * @param wait - デバウンスの待機時間 (ミリ秒)
- * @param isTyping - 入力中の状態を管理する関数 (Svelte のストアなど)
+ * @param isTyping - 入力中の状態を管理するオブジェクト
+ * @param isTyping.set - 入力中状態を更新するメソッド (true: 入力中, false: 入力終了)
  * @returns キーアップイベント処理関数
  */
-export function DebounceKeyUp(input: (e: KeyboardEvent)=> void,	wait: number,	isTyping: { set: (value: boolean)=> void }) {
+export function DebounceKeyUp(
+	input: (e: KeyboardEvent)=> void,
+	wait: number,
+	isTyping: { set: (value: boolean)=> void },
+) {
 	let typingTimeout
 
 	const debouncedInput = debounce((e: KeyboardEvent) => {
