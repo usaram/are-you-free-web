3c6350d00f6fcecafed8361ca756d26af4f6edf3
Masaru Nohara <masaru.nohara@tohmatsu.co.jp>
2025-04-06 21:11:16 +0900

    [add] time table

--- src/lib/utils/debounce/DebounceOnClick.ts
+++ src/lib/utils/debounce/DebounceOnClick.ts
@@ -4,20 +4,25 @@
  *
  * @param func - クリックイベント時に実行する関数 (非同期関数も可)
  * @param wait - デバウンスの待機時間 (ミリ秒)
- * @param isLoading - ローディング状態を管理するオブジェクト。`set` メソッドで状態を更新します。
+ * @param isLoading - ローディング状態を管理するオブジェクト
+ * @param isLoading.set - ローディング状態を更新するメソッド (true: ローディング開始, false: ローディング終了)
  * @returns クリックイベント処理関数
  */
 export function DebounceOnClick(
 	func: (e: MouseEvent)=> void | Promise<void>,
 	wait: number,
-	isLoading: { set: (value: boolean)=> void },
-) {
-	const debouncedOnClick = debounce(async (e: MouseEvent) => {
-		await func(e) // デバウンスされたクリック処理関数
-	}, wait)
+	isLoading: { set: (state: boolean)=> void },
+): (e: MouseEvent)=> void {
+	let timeout: NodeJS.Timeout | null = null
 
 	return (e: MouseEvent) => {
-		isLoading.set(true) // ローディング状態を設定
-		debouncedOnClick(e)
+		if (timeout)
+			clearTimeout(timeout)
+
+		isLoading.set(true)
+		timeout = setTimeout(async () => {
+			await func(e)
+			isLoading.set(false)
+		}, wait)
 	}
 }
