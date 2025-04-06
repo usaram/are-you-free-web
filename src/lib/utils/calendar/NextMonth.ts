a4018791848b6d9eeda1a1ee60c154e1bf419995
Masaru Nohara <masaru.nohara@tohmatsu.co.jp>
2025-04-06 21:13:41 +0900

    [fix] eslint

--- src/lib/utils/calendar/NextMonth.ts
+++ src/lib/utils/calendar/NextMonth.ts
@@ -2,7 +2,6 @@
  * @description この関数は、カレンダーで次の月に移動するために使用されます。
  * @param monthOffset - 現在の月からのオフセット（月数）。
  * @param calendarDisplayMonths - カレンダーに表示する月数。
- * @returns none
  * @example
  * // 現在の月から次の月に移動
  * NextMonth(1, 12);
--- src/lib/utils/calendar/PrevMonth.ts
+++ src/lib/utils/calendar/PrevMonth.ts
@@ -1,7 +1,6 @@
 /**
  * @description この関数は、カレンダーで前の月に移動するために使用されます。
  * @param calendarOffset - 現在の月からのオフセット（月数）。
- * @returns none
  * @example
  * // 現在の月から前の月に移動
  * PrevMonth(1);
