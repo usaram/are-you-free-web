4687adebf845ff3b830f303eccb2502a0daf2eaa
Masaru Nohara <masaru.nohara@tohmatsu.co.jp>
2025-04-06 21:16:56 +0900

    [fix] eslint

--- src/lib/components/calendar/NextMonth.ts
+++
@@ -1,7 +0,0 @@
-// Navigate to next month (but not after month+2)
-function nextMonth(monthOffset: number) {
-	if (monthOffset < 2) {
-		monthOffset++
-		updateCalendar()
-	}
-}
--- src/lib/components/calendar/PrevMonth.ts
+++
@@ -1,7 +0,0 @@
-// Navigate to previous month (but not before current month)
-function prevMonth(monthOffset: number) {
-	if (monthOffset > 0) {
-		monthOffset--
-		updateCalendar()
-	}
-}
